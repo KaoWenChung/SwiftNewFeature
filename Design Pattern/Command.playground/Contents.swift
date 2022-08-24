1ad963df5af7894963e1a578d9a497b1895d2670
KaoWenChung <k022298@gmail.com>
2022-08-24 16:49:24 +0800

    Implemented Command design pattern

--- Design Pattern/Command.playground/Contents.swift
+++ Design Pattern/Command.playground/Contents.swift
@@ -2,7 +2,88 @@
 
 // In Command Design Pattern,
 // There are three characters in this pattern
+// The conversion allows deferred or remote execution of commands, storing command history, etc.
 //  - Invoker: Stores and excutes commands
 //  - Command: An object that encapsulates action
 //  - Receiver: an object acted upon by command
 
+
+protocol Command {
+    func execute()
+}
+
+class SimpleCommand: Command {
+    
+    private let payload: String
+    
+    init(_ payload: String) {
+        self.payload = payload
+    }
+
+    func execute() {
+        print("SimpleCommand: handle with print:", payload)
+    }
+}
+
+class ComplexCommand: Command {
+    private let receiver: Receiver
+    
+    private let a: String
+    private let b: String
+    
+    init(_ receiver: Receiver, a: String, b: String) {
+        self.receiver = receiver
+        self.a = a
+        self.b = b
+    }
+    
+    func execute() {
+        receiver.handleWith(aTask: a)
+        receiver.handleWith(bTask: b)
+    }
+}
+
+protocol Receiver {
+    func handleWith(aTask: String)
+    func handleWith(bTask: String)
+}
+
+class SimpleReceiver: Receiver {
+    
+    func handleWith(aTask: String) {
+        print("SimpleReceiver handle with", aTask)
+    }
+    
+    func handleWith(bTask: String) {
+        print("SimpleReceiver handle with", bTask)
+    }
+    
+}
+
+class Invoker {
+
+    private var onStart: Command?
+    private var onFinish: Command?
+
+    func setOnStart(_ command: Command) {
+        onStart = command
+    }
+
+    func setOnFinish(_ command: Command) {
+        onFinish = command
+    }
+
+    func executeTask() {
+        print("Invoker execute onStart")
+        onStart?.execute()
+        print("Invoker execute onFinish")
+        onFinish?.execute()
+    }
+}
+
+let invoker: Invoker = Invoker()
+invoker.setOnStart(SimpleCommand("Hello!!"))
+
+let receiver: SimpleReceiver = SimpleReceiver()
+invoker.setOnFinish(ComplexCommand(receiver, a: "Send email", b: "Save report"))
+invoker.executeTask()
