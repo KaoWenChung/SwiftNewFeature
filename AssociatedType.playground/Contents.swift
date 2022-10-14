b98c74835144949084b04c10274429669cdddb41
wyn <k022298@gmail.com>
2022-10-14 14:21:50 +0100

    Add Associate type

---
+++ AssociatedType.playground/Contents.swift
@@ -0,0 +1,8 @@
+/*
+// Reference: https://www.avanderlee.com/swift/associated-types-protocols/#what-is-an-associated-type
+ 
+What is an associated type?
+
+An associated type can be seen as a replacement of a specific type within a protocol definition. In other words: it’s a placeholder name of a type to use until the protocol is adopted and the exact type is specified.
+*/
+
