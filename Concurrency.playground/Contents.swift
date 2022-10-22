5c9967c90333ff85484490e6587006223de83a40
wyn <k022298@gmail.com>
2022-10-22 19:16:28 +0100

    Add concurrency content

--- Concurrency.playground/Contents.swift
+++ Concurrency.playground/Contents.swift
@@ -1,1 +1,56 @@
+// await async
 
+import UIKit
+
+enum Result<Value> {
+
+    case success(Value)
+    case failure(Error)
+
+}
+
+enum RepositoryError: Error {
+    case noData
+}
+
+class ViewController {
+    let viewModel = ViewMdoel()
+    func viewDidLoad() {
+        print("Test 1")
+        Task.init() {
+            do {
+                let data = try await viewModel.fetchData()
+                print("You have gotten", data)
+            } catch {
+                print("You encounter an error:", error)
+            }
+            
+        }
+        print("Test 2")
+    }
+}
+
+class ViewMdoel {
+    private let apiService = Repository()
+    func fetchData() async throws -> String {
+        // throw Error
+//        throw RepositoryError.noData
+        // Success
+        let data = try await apiService.fetchData()
+        return data
+    }
+}
+
+class Repository {
+    func fetchData() async throws -> String {
+        // Mock request time
+        sleep(1)
+        return "Data"
+    }
+}
+
+
+
+
+
+ViewController().viewDidLoad()
